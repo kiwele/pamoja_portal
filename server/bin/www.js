51f173a122eb1e85312de4360ab3373dc4bb9f2c
kiwele <musakiwele@gmail.com>
2023-07-08 18:18:20 +0300

    changed port

--- server/bin/www.js
+++ server/bin/www.js
@@ -12,7 +12,7 @@ import app from '../app.js';
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '4000');
+const port = normalizePort(process.env.PORT || '5000');
 app.set('port', port);
 
 /**
