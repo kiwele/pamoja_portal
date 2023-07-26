0b4cd1d58c7920a639ecbc3b65f4bbb3a30c9bf3
kiwele <musakiwele@gmail.com>
2023-07-26 20:59:18 +0300

    minor changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = 'http://13.48.59.172:5000/'
-const BASE_URL = 'http://localhost:5000/'
+const BASE_URL = 'http://13.48.59.172:5000/'
+// const BASE_URL = 'http://localhost:5000/'
 
 
 export default axios.create({
